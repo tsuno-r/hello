9d5889f7dceb63e519e10be4cb4b92b0f9940b85
retatti <retareta0117@yahoo.co.jp>
2020-06-30 21:51:41 +0900

    数あてゲームの修正

--- kazuate/src/app/App.java
+++ kazuate/src/app/App.java
@@ -1,13 +1,25 @@
 package app;
 import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int num = 50;
-        int cnt = 0;
+        kazuate();
+    }
+
+    // 数字あてゲーム
+    public static void kazuate() {
+        int num; // 推測する数字
+        int cnt = 0; // 現在の入力回数
+        int limit = 5; // 入力上限
         int input;
+        Random random = new Random();
+
+        // 推測する数字(10~99)の生成
+        num = random.nextInt(90) + 10;
+        
     
-        while(cnt < 5) {
+        while(cnt < limit) {
             System.out.println(String.valueOf(cnt + 1) + "回目");
             System.out.print("数字を入力してください:");
             Scanner scan = new Scanner(System.in);
